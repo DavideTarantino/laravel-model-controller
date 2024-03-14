3c0aaabe0106742c06e2bfb7f26c612639c43265
DavideTarantino <davidetarantino2005@gmail.com>
2024-03-14 15:59:03 +0100

    stampa a schermo degli elementi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,14 @@
 
 @section('title', 'Benvenuto | Home')
 
-@foreach ($movie as $item)
-    <div>
-        <h3></h3>
-    </div>
-@endforeach
+    <main>
+        @foreach ($movie as $item)
+        <div>
+            <h3>Titolo: {{ $item['title'] }}</h3>
+            <h3>Titolo Originale: {{ $item['original_title'] }}</h3>
+            <h4>Nazionalita: {{ $item['nationality'] }}</h4>
+            <h4>Data di Pubblicazione: {{ $item['date'] }}</h4>
+            <h4>Voto: {{ $item['vote'] }}</h4>
+        </div>
+        @endforeach
+    </main>
